e533c95464415de61297e888a897699329c9e001
Jessi Breen <jessibreen@gmail.com>
2020-02-24 20:46:36 -0500

    changed tileLayer method

--- js/map.js
+++ js/map.js
@@ -17,8 +17,14 @@
         map.createPane("pane600").style.zIndex = 600; // marker pane
                
     //Create the baselayer and add it to the map
-        var layer =new L.StamenTileLayer("toner-lite");
-        map.addLayer(layer);     
+    var layer = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	subdomains: 'abcd',
+	minZoom: 0,
+	maxZoom: 20,
+	ext: 'png'
+});
+map.addLayer(layer);
         
     //Create the census tract layer from the geojson
        $.getJSON("data/focustracts.geojson", function(data) {
